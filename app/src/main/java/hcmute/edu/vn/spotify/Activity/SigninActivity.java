bb2f74d75dfc55a0c8a4881443e669df8e46d729
ThienHoanNeee <softteam2021@gmail.com>
2022-05-01 15:47:00 +0700

    [Update] add sign in and sign up activity

---
+++ app/src/main/java/hcmute/edu/vn/spotify/Activity/SigninActivity.java
@@ -0,0 +1,16 @@
+package hcmute.edu.vn.spotify.Activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import hcmute.edu.vn.spotify.R;
+
+public class SigninActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
