703e15885ecdc43d36817c0c563447df76850034
Nguyễn Tấn Đạt <76637214+NguyenTanDatDF@users.noreply.github.com>
2022-05-15 15:02:36 +0700

    [Update] update player control bar, it can slide right left

--- app/src/main/java/hcmute/edu/vn/spotify/Activity/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/spotify/Activity/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.google.android.exoplayer2.ExoPlayerFactory;
@@ -22,6 +25,7 @@ import hcmute.edu.vn.spotify.Fragment.LibraryFragment;
 import hcmute.edu.vn.spotify.Fragment.SearchFragment;
 import hcmute.edu.vn.spotify.Fragment.HomeFragment;
 import hcmute.edu.vn.spotify.R;
+import hcmute.edu.vn.spotify.Service.OnSwipeTouchListener;
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
 
@@ -35,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavigation = findViewById(R.id.meow_bot_nav);
-
+        ImageButton imgv_heart = findViewById(R.id.imgv_heart);
 
         bottomNavigation.setCircleColor(2);
         bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_home));
@@ -88,6 +92,32 @@ public class MainActivity extends AppCompatActivity {
 
         String url = "https://firebasestorage.googleapis.com/v0/b/algebraic-fin-332903.appspot.com/o/y2mate.com%20-%20Mixtape%20%C4%90i%20C%C3%B9ng%20N%C4%83m%20Th%C3%A1ng%20%20TayNguyenSound%20Full.mp3?alt=media&token=b36c5b88-9e24-4d18-b490-4f37328dd34a";
         PlayMedia(url);
+
+        final int[] favorite = {0};
+        imgv_heart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(favorite[0] == 0)
+                {
+                    imgv_heart.setImageResource(R.drawable.ic_heart_fill);
+                    favorite[0] = 1;
+                }
+                else
+                {
+                    imgv_heart.setImageResource(R.drawable.ic_not_heart);
+                    favorite[0] = 0;
+                }
+            }
+        });
+
+        pvMain.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+            public void onSwipeRight() {
+                Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeLeft() {
+                Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     private void replace(Fragment fragment) {
     FragmentTransaction fragmentTransaction = getSupportFragmentManager ().beginTransaction();
